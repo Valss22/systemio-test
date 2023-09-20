787353b628267e745cce2dc2dd403739a0343092
vladislavshokorov <valsshokorov@gmail.com>
2023-09-20 15:50:31 +0600

    payment processors added

---
+++ src/PaypalPaymentProcessor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Exception;
+
+class PaypalPaymentProcessor
+{
+    /**
+     * @param int $price payment amount in smallest currency unit
+     * @throws Exception in case of a failed payment
+     */
+    public function pay(int $price): void
+    {
+        if ($price > 100000) {
+            throw new Exception('Too high price');
+        }
+
+        //process payment logic
+    }
+}
